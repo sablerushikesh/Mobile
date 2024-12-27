c8d5e7e384c2db89b108cac60094982377e51c36
rushi <rushi@DESKTOP-KASF1I7>
2024-12-27 04:56:21 -0800

    by rushi

---
+++ MobileIphone/src/com/cjc/A.java
@@ -0,0 +1,21 @@
+package com.cjc;
+
+public class A {
+	
+	public void m1()
+	{
+			System.out.println("M1 Iphone");
+	}
+	public void m2()
+	{
+		System.out.println("m2 Samsung");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A a = new A();
+		a.m1();
+		a.m2();
+	}
+
+}
